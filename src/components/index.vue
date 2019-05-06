3101c81b25b110c1991c73ae1b76026726e77c66
Your Name <you@example.com>
2019-05-06 15:14:16 +0800

    整合嵌套路由

--- src/components/index.vue
+++ src/components/index.vue
@@ -22,18 +22,22 @@
             class="el-menu-vertical-demo"
             @open="handleOpen"
             @close="handleClose"
+            router
           >
             <el-submenu index="1">
               <template slot="title">
                 <i class="el-icon-location"></i>
                 <span>导航一</span>
               </template>
-                <el-menu-item index="1-1"><i class="el-icon-menu"></i>选项1</el-menu-item>
+                <el-menu-item index="users"><i class="el-icon-menu"></i>选项1</el-menu-item>
                 <el-menu-item index="1-2"><i class="el-icon-menu"></i>选项2</el-menu-item>
             </el-submenu>
           </el-menu>
         </el-aside>
-        <el-main class="main">Main</el-main>
+        <el-main class="main">
+            <!-- router-view -->
+            <router-view></router-view>
+        </el-main>
       </el-container>
     </el-container>
   </div>
--- src/router.js
+++ src/router.js
@@ -5,12 +5,16 @@ Vue.use(VueRouter)  //use一下
 // 导入组件
 import login from './components/login.vue'
 import index from './components/index.vue'
+// 导入嵌套组件
+import users from './components/users.vue'
 
 // 路由规则
 const routes = [
     // { path: '/', redirect: '/index' },
     {path:'/login',component:login},
-    {path:'/',component:index},
+    {path:'/',component:index,children:[
+        {path:'users',component:users}  //嵌套路由不需要写 '/'
+    ]},
 
 ]
 
