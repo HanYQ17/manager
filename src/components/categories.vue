a926d9d9ce503e59684650d933e0733b4aa09a7f
Your Name <you@example.com>
2019-05-12 22:25:45 +0800

    categories渲染数据 表格展开 设置row-key

--- src/components/categories.vue
+++ src/components/categories.vue
@@ -15,10 +15,24 @@
     </el-row>
 
     <!-- 表格 -->
-    <el-table :data="tableData" style="width: 100%">
-      <el-table-column prop="date" label="日期" width="180"></el-table-column>
-      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-      <el-table-column prop="address" label="地址"></el-table-column>
+    <el-table :data="tableData" style="width: 100%" row-key="cat_id">
+      <el-table-column prop="cat_name" label="分类名称" width="180"></el-table-column>
+      <el-table-column label="级别" width="180">
+        <template slot-scope="scope">
+          {{scope.row.cat_level==0?'一级':''}}
+          {{scope.row.cat_level==1?'二级':''}}
+          {{scope.row.cat_level==2?'三级':''}}
+        </template>
+      </el-table-column>
+      <el-table-column label="是否有效">
+        <template slot-scope="scope">{{scope.row.cat_deleted==false?'有效':'无效'}}</template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template>
+          <el-button type="primary" icon="el-icon-edit" plain size="mini"></el-button>
+          <el-button type="danger" icon="el-icon-delete" plain size="mini"></el-button>
+        </template>
+      </el-table-column>
     </el-table>
 
     <!-- 分页 -->
@@ -37,38 +51,24 @@ export default {
   name: "categories",
   data() {
     return {
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄"
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄"
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄"
-        }
-      ]
+      tableData: [] //表格数据
     };
+  },
+  created() {
+    // 获取数据
+    this.$request.getCategories().then(res => {
+      console.log(res);
+      this.tableData = res.data.data;
+    });
   }
 };
 </script>
 
 <style lang="scss">
-.my_breadcrumb{
-    height: 45px;
-    line-height: 45px;
-    background-color: #d3dce6;
-    padding-left: 10px;
+.my_breadcrumb {
+  height: 45px;
+  line-height: 45px;
+  background-color: #d3dce6;
+  padding-left: 10px;
 }
 </style>
